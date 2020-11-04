d814c2fe513db5e70237d94c612bdeb85a9d8cdd
HM\PIPRA <pintu.paul@hm.com>
2020-11-04 15:36:34 +0100

    - added Item class

--- src/main/java/com/example/Webshop/Item.java
+++ src/main/java/com/example/Webshop/Item.java
@@ -1,2 +1,40 @@
-package com.example.Webshop;public class Item {
+package com.example.Webshop;
+
+public class Item {
+    private String name;
+    private String description;
+    private int price;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
