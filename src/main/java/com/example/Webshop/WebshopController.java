cff8c17ff68c51f1ac11b5604168f1f3db660211
HM\PIPRA <pintu.paul@hm.com>
2020-11-05 08:22:41 +0100

    - Added Getmapping for the landing page in Webshop controller - Moved MH logo to static folder as it was not visible while running the appliaction

--- src/main/java/com/example/Webshop/WebshopController.java
+++ src/main/java/com/example/Webshop/WebshopController.java
@@ -1,4 +1,12 @@
 package com.example.Webshop;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebshopController {
+    @GetMapping("/")
+    String welcome(){
+        return "landingPage";
+    }
 }
